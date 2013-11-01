3151997534a869b8b6e201e3350527d665777be5
HHadi <hhamoud@mail.csuchico.edu>
2013-10-31 23:23:51 -0700

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+user = User.create ([{email: "hadi@gmail.com",   password: "hellohello",password_confirmation: "hellohello"},
+					 {email: "hamoud@gmail.com", password: "hellohello",password_confirmation: "hellohello"}])
+
 doi = Doi.create([{name: "my first doi dddd", description: "hello ddd"},
-				  {name: "my second doi dddd", description: "this is the second doi"}])
+				  {name: "my seco doi dddd",  description: "this is the second doi"}])
+
 
 url = Url.create([{url: "first url"},
 				  {url: "second url"}])
+
+review = Review.create([{review: "first review"},
+				        {review: "second review"}])
